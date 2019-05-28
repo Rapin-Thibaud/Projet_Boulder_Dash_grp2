afc739e1ebb55f0f4578f0511491a7843ad1dcfc
Kitawiim <49398010+Kitawiim@users.noreply.github.com>
2019-05-28 10:45:31 +0200

    Update View.java

--- view/src/main/java/view/View.java
+++ view/src/main/java/view/View.java
@@ -16,72 +16,68 @@ import contract.IView;
  */
 public final class View implements IView, Runnable {
 
-    /**
-     * Key code to controller order.
-     *
-     * @param keyCode
-     *            the key code
-     * @return the controller order
-     */
-    protected static ControllerOrder keyCodeToControllerOrder(final int keyCode) {
-        switch (keyCode) {
-        case KeyEvent.VK_G:
-            return ControllerOrder.English;
-        case KeyEvent.VK_F:
-            return ControllerOrder.Francais;
-        case KeyEvent.VK_D:
-            return ControllerOrder.Deutsch;
-        case KeyEvent.VK_I:
-            return ControllerOrder.Indonesia;
-        default:
-            return ControllerOrder.English;
-        }
-    }
+	/** The frame. */
+	private final ViewFrame viewFrame;
 
-    /** The frame. */
-    private final ViewFrame viewFrame;
+	/**
+	 * Instantiates a new view.
+	 *
+	 * @param model
+	 *          the model
+	 */
+	public View(final IModel model) {
+		this.viewFrame = new ViewFrame(model);
+		SwingUtilities.invokeLater(this);
+	}
 
+	/**
+	 * Key code to controller order.
+	 *
+	 * @param keyCode
+	 *          the key code
+	 * @return the controller order
+	 */
+	protected static ControllerOrder keyCodeToControllerOrder(final int keyCode) {
+		switch (keyCode) {
+			case KeyEvent.VK_UP:
+				return ControllerOrder.UP;
+			case KeyEvent.VK_DOWN:
+				return ControllerOrder.DOWN;
+			case KeyEvent.VK_RIGHT:
+				return ControllerOrder.RIGHT;
+			case KeyEvent.VK_LEFT:
+				return ControllerOrder.LEFT;
+			default:
+				return ControllerOrder.NOTHING;
+		}
+	}
 
-    /**
-     * Instantiates a new view.
-     *
-     * @param model
-     *            the model
-     */
-    public View(final IModel model) {
-        this.viewFrame = new ViewFrame(model);
-        SwingUtilities.invokeLater(this);
-    }
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see contract.IView#printMessage(java.lang.String)
+	 */
+	public void printMessage(final String message) {
+		this.viewFrame.printMessage(message);
+	}
 
- 
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run() {
+		this.viewFrame.setVisible(true);
+	}
 
-    /*
-     * (non-Javadoc)
-     *
-     * @see contract.IView#printMessage(java.lang.String)
-     */
-    @Override
-    public void printMessage(final String message) {
-        this.viewFrame.printMessage(message);
-    }
-
-    /*
-     * (non-Javadoc)
-     *
-     * @see java.lang.Runnable#run()
-     */
-    @Override
-    public void run() {
-        this.viewFrame.setVisible(true);
-    }
-
-    /**
-     * Sets the controller.
-     *
-     * @param controller
-     *            the new controller
-     */
-    public void setController(final IController controller) {
-        this.viewFrame.setController(controller);
-    }
+	/**
+	 * Sets the controller.
+	 *
+	 * @param controller
+	 *          the new controller
+	 */
+	public void setController(final IController controller) {
+		this.viewFrame.setController(controller);
+	}
 }
+
