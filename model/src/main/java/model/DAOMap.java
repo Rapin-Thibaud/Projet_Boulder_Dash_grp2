0951a02bfa74ad31c0958f23e4a5022d50149f36
Rapin-Thibaud <44433529+Rapin-Thibaud@users.noreply.github.com>
2019-05-28 10:12:35 +0200

    test

--- model/src/main/java/model/DAOMap.java
+++ model/src/main/java/model/DAOMap.java
@@ -14,99 +14,91 @@ import entity.Map;
  */
 class DAOMap extends DAOEntity<Map> {
 
-	/**
-	 * Instantiates a new DAO hello world.
-	 *
-	 * @param connection
-	 *          the connection
-	 * @throws SQLException
-	 *           the SQL exception
-	 */
-	public DAOMap(final Connection connection) throws SQLException {
-		super(connection);
-	}
+    public DAOMap(final Connection connection) throws SQLException {
+        super(connection);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see model.DAOEntity#create(model.Entity)
-	 */
-	@Override
-	public boolean create(final Map entity) {
-		// Not implemented
-		return false;
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see model.DAOEntity#create(model.Entity)
+     */
+    @Override
+    public boolean create(final Map entity) {
+        // Not implemented
+        return false;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see model.DAOEntity#delete(model.Entity)
-	 */
-	@Override
-	public boolean delete(final Map entity) {
-		// Not implemented
-		return false;
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see model.DAOEntity#delete(model.Entity)
+     */
+    @Override
+    public boolean delete(final Map entity) {
+        // Not implemented
+        return false;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see model.DAOEntity#update(model.Entity)
-	 */
-	@Override
-	public boolean update(final Map entity) {
-		// Not implemented
-		return false;
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see model.DAOEntity#find(int)
+     */
+    @Override
+    public Map find(final int id) {
+        Map map = new Map();
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see model.DAOEntity#find(int)
-	 */
-	@Override
-	public Map find(final int id) {
-		Map map = new Map();
+        try {
+            final String sql = "{call helloworldById(?)}";
+            final CallableStatement call = this.getConnection().prepareCall(sql);
+            call.setInt(1, id);
+            call.execute();
+            final ResultSet resultSet = call.getResultSet();
+            if (resultSet.first()) {
+                map = new Map(id, resultSet.getString("code"), resultSet.getString("message"));
+            }
+            return map;
+        } catch (final SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-		try {
-			final String sql = "{call helloworldById(?)}";
-			final CallableStatement call = this.getConnection().prepareCall(sql);
-			call.setInt(1, id);
-			call.execute();
-			final ResultSet resultSet = call.getResultSet();
-			if (resultSet.first()) {
-				map = new Map(id, resultSet.getString("code"), resultSet.getString("message"));
-			}
-			return map;
-		} catch (final SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see model.DAOEntity#find(java.lang.String)
+     */
+    @Override
+    public Map find(final String code) {
+        Map map = new Map();
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see model.DAOEntity#find(java.lang.String)
-	 */
-	@Override
-	public Map find(final String code) {
-		Map map = new Map();
+        try {
+            final String sql = "{call helloworldByCode(?)}";
+            final CallableStatement call = this.getConnection().prepareCall(sql);
+            call.setString(1, code);
+            call.execute();
+            final ResultSet resultSet = call.getResultSet();
+            if (resultSet.first()) {
+                map = new Map(resultSet.getInt("id"), code, resultSet.getString("message"));
+            }
+            return map;
+        } catch (final SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-		try {
-			final String sql = "{call helloworldByCode(?)}";
-			final CallableStatement call = this.getConnection().prepareCall(sql);
-			call.setString(1, code);
-			call.execute();
-			final ResultSet resultSet = call.getResultSet();
-			if (resultSet.first()) {
-				map = new Map(resultSet.getInt("id"), code, resultSet.getString("message"));
-			}
-			return map;
-		} catch (final SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see model.DAOEntity#update(model.Entity)
+     */
+    @Override
+    public boolean update(final Map entity) {
+        // Not implemented
+        return false;
+    }
 
 }
