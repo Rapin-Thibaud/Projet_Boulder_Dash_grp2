b3f6cdafa6eb0c86d1f7b1f49e6b8d933b019bef
Inès Casenave <ines.ath.casenave@gmail.com>
2019-06-03 14:25:37 +0200

    diamond

--- model/src/main/java/model/element/mobile/Diamond.java
+++ model/src/main/java/model/element/mobile/Diamond.java
@@ -1,11 +1,43 @@
 package model.element.mobile;
 
+import model.element.BehaviorType2;
 import model.element.Sprite;
 
-public class Diamond {
-    private static final Sprite sprite = new Sprite();
+public class Diamond extends BehaviorType2{
+    @SuppressWarnings("unused")
+	private static final Sprite sprite = new Sprite();
+	private int x, y;
 
-    public Diamond() {
-// jbpiub
+	boolean toBePickable = true;
+	
+	public void toPickDiamond() {
+        if (this.toBePickable = true) {
+
+        }
+	}
+    
+    Diamond(final int x, final int y){
+        this.setX(x);
+        this.setY(y);
+    }
+   
+    public int getX() {
+        return this.x;
+    }
+
+    public void setX(final int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setY(final int y) {
+        this.y = y;
+    }
+	public Diamond() {
+    	this.toFall();
+        this.toFallAndKill();
     }
 }
